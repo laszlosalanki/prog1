6a181a7c89fd24b0244cec58ceffd6f7e45e13b9
laszlosalanki <55296085+laszlosalanki@users.noreply.github.com>
2020-02-27 15:33:06 +0100

    Add files via upload

---
+++ unaris.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int dec;
+  printf("Add meg a decimális számot: ");
+  scanf("%d",&dec);
+  for (int i = 0; i < dec; i++) {
+    printf("1");
+  }
+  printf("\n");
+  return 0;
+}
